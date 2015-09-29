2c196187380ba8b0c147ed5dc1baa88a7d07e4be
Weston Beard <weston.beard@gmail.com>
2015-09-29 16:16:19 -0400

    adding Choice object to admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,13 +2,19 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,  {'fields': ['question_text']}),
         ('Date informaiton', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
+
 
