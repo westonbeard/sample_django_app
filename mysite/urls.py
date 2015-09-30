8127f5a7966858d5fbe057fcf9d47a27456b2e06
Weston Beard <weston.beard@gmail.com>
2015-09-30 14:15:18 -0400

    adding more views

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
+
 from django.contrib import admin
 
+
 urlpatterns = [
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
