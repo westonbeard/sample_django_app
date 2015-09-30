1ea3d7ab59bf583d258fb28fb7af97dc5eeadffa
Weston Beard <weston.beard@gmail.com>
2015-09-30 17:36:51 -0400

    adding results template

--- polls/views.py
+++ polls/views.py
@@ -21,7 +21,6 @@ def results(request, question_id):
     return render(request, 'polls/results.html', {'question': question})
 
 
-
 def vote(request, question_id):
     p = get_object_or_404(Question, pk=question_id)
     try:
